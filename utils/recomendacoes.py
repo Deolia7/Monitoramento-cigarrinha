a4aadd768dc98c6f576d96667f66e09ac604bd82
Deolia7 <alan.agrogsia@gmail.com>
2025-03-26 19:42:04 -0300

    Delete utils directory

--- utils/recomendacoes.py
+++
@@ -1,7 +0,0 @@
-def gerar_recomendacoes(previsao_df):
-    picos = previsao_df[(previsao_df["adultos"] >= 10) | (previsao_df["ninfas"] >= 8)]
-    if picos.empty:
-        return "População sob controle. Continue monitorando regularmente."
-    else:
-        pico = picos.iloc[0]["data"]
-        return f"🚨 Recomendado aplicar inseticida antes de {pico}, para evitar o pico populacional."
