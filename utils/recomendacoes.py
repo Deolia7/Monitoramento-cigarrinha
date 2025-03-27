a00672f0dc6b0b2acf5d57f405d7df68de016d87
Deolia7 <alan.agrogsia@gmail.com>
2025-03-26 21:59:29 -0300

    Delete utils directory

--- utils/recomendacoes.py
+++
@@ -1,8 +0,0 @@
-def gerar_recomendacoes(adultos, ninfas):
-    total = adultos + ninfas
-    if total >= 6:
-        return "Aplicar inseticida imediatamente para conter a infestação."
-    elif 3 <= total < 6:
-        return "Monitorar diariamente. Considerar aplicação se o clima for favorável à praga."
-    else:
-        return "População sob controle. Reavaliar em 3 dias."
