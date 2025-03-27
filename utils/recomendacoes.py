dffaf44e84927434702c04105a944e3b4da2fd78
Deolia7 <alan.agrogsia@gmail.com>
2025-03-26 21:10:31 -0300

    Delete utils directory

--- utils/recomendacoes.py
+++
@@ -1,5 +0,0 @@
-
-def gerar_recomendacoes(media_adultos, media_ninfas):
-    if media_adultos + media_ninfas > 10:
-        return "Aplicar inseticida imediatamente para evitar o pico populacional."
-    return "Monitorar mais alguns dias antes da aplicação."
